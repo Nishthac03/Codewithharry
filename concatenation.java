83dbd5c414b397467543a9d2c8d2770d6905c923
Nishtha Chopra <117111893+Nishthac03@users.noreply.github.com>
2023-03-25 19:49:26 +0530

    Create concatenation.java

---
+++ concatenation.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class cwh_15ps3
+{
+    public static void main(String[] args)
+    {
+        /*
+        problem 3:
+         */
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER YOUR NAME:");
+        String a=sc.nextLine();
+        System.out.println("Dear "+a+", thanks a lot");
+    }
+}
